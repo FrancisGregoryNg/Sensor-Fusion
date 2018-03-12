df5efb2f1ed56c84d1234075e075505f02747bd4
FrancisGregoryNg <francisng2718@gmail.com>
2018-03-13 02:20:59 +0800

    rough outline

--- particle_filter.py
+++ particle_filter.py
@@ -2,33 +2,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 from numpy.random import uniform 
 
-N = 20000  # number of points
-radius = 1
-area = (2*radius)**2
+#------Initialization-----
+#Set the number of particles
+N = 20000
 
+#Draw samples from a uniform distribution (initial distribution)
 pts = uniform(-1, 1, (N, 2))
 
-# distance from (0,0) 
-dist = np.linalg.norm(pts, axis=1)
-in_circle = dist <= 1
+#Compute the weights
 
-pts_in_circle = np.count_nonzero(in_circle)
-pi = area * (pts_in_circle / N)
+#Normalize the weights
 
-# plot results
-plt.scatter(pts[in_circle,0], pts[in_circle,1], 
-            marker=',', edgecolor='k', s=1)
-plt.scatter(pts[~in_circle,0], pts[~in_circle,1], 
-            marker=',', edgecolor='r', s=1)
-plt.axis('equal')
-plt.show()
+#-----Main Loop (Iterations)-----
+#Use importance distribution
 
-print('mean pi(N={})= {:.4f}'.format(N, pi))
-print('err  pi(N={})= {:.4f}'.format(N, np.pi-pi))
+#Update weights
 
-words=['aloha','somebody','come']
-for w in words[:]:
-    if len(w)>5:
-        words.insert(0, w)
-        
-print(words)
+#Normalize weights
+
+#Conduct resampling
