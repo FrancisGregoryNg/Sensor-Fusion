686e36dd5631937b36396bd51a6eaa0c16b3be9e
Francis Gregory Ng <francisng2718@gmail.com>
2018-03-30 22:35:55 +0800

    slight edit, not yet working
    must:
        iterate using range of integers
        open csv as writable

--- IEEE.py
+++ IEEE.py
@@ -1,5 +1,6 @@
 import csv
 import pigpio
+import numpy as np
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #-------------------------      Initialization     ----------------------------
@@ -9,15 +10,19 @@ import pigpio
 pi = pigpio.pi()
 
 # Set the size of the room (must be the same with particle_filter.py)
+# the room dimensions (width, length) correspond to (x, y)
+# the origin starts at a selected corner of the room
+# the measurement unit is in millimeters (mm)
 room_width = 5000
 room_length = 5000
 
 # The balance between precision and difficulty is determined heuristically
+# the measurement unit is in millimeters (mm)
 grid_precision = 100
 
 # The size is determined inclusive of the origin which is located in a corner
-size_of_x = 1 + room_width/grid_precision
-size_of_y = 1 + room_length/grid_precision
+size_of_x = int(1 + np.floor(room_width/grid_precision))
+size_of_y = int(1 + np.floor(room_length/grid_precision))
 
 # Calculate the number of datapoints for use in the signal mapping
 number_of_datapoints = size_of_x * size_of_y
@@ -26,7 +31,7 @@ number_of_datapoints = size_of_x * size_of_y
 #-------------------------      Grid Locations     ----------------------------
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Start with the 
+# Start with the origin
 x, y = 0, 0
 
 # Monitor the reversals because the progression is done in a snake-like manner
@@ -34,13 +39,13 @@ direction = 1
 counter = 0
 
 # Set the grid locations
-with open('IEEE_matching_locations.csv') as locations:
+with open('IEEE_matching_locations.csv', 'w') as locations:
     coordinates = ['x', 'y']
     write_locations = csv.DictWriter(locations, fieldnames = coordinates)
     write_locations.writeheader()
 
 # Make the grid locations using snake-like progression
-for run in number_of_datapoints:
+for run in range(number_of_datapoints):
     if counter < size_of_x:
         x = x + direction
     else:
@@ -64,12 +69,12 @@ pi.bb_serial_read_open(15, 115200)
 log_data = 0
 
 # Log the data directly onto the .csv file
-with open('IEEE_signal_database.csv') as database:
+with open('IEEE_signal_database.csv', 'w') as database:
     sensors = ['IEEE_0', 'IEEE_1']
     write_database = csv.DictWriter(database, fieldnames = sensors)
     write_database.writeheader()
 
-for run in number_of_datapoints:
+for run in range(number_of_datapoints):
     log_data = 0 # specify pin input in the future
     if log_data == 1:
         run = run + 1
