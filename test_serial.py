115ad275ed7f1b6e7b313a4c4ad1aa30141b94c9
Francis Gregory Ng <francisng2718@gmail.com>
2018-04-16 14:00:33 +0800

    added serial_read_close
    serial_read_close is needed so that redoing serial_read_open will not throw out an error

--- test_serial.py
+++ test_serial.py
@@ -6,8 +6,8 @@ pi = pigpio.pi()
 # Open pins for bit bang reading of serial data
 # For IEEE_0: (board 16, BCM 23)
 # For IEEE_1: (board 18, BCM 24)
-# pi.bb_serial_read_open(14, 9600) --> outputs an error, apparently not needed
-# pi.bb_serial_read_open(15, 9600) --> outputs an error, apparently not needed 
+pi.bb_serial_read_open(14, 9600)
+pi.bb_serial_read_open(15, 9600)
 
 (IEEE_0_start, temp_data_0) = pi.bb_serial_read(23)
 (IEEE_1_start, temp_data_1) = pi.bb_serial_read(24) 
@@ -17,3 +17,6 @@ for run in range(0, 5):
     print("temp_data_0: " + str(temp_data_0) + "\n")
     print("IEEE_1_start: " + str(IEEE_1_start) + "\n")
     print("temp_data_1: " + str(temp_data_1) + "\n")
+    
+pi.bb_serial_read_close(14)
+pi.bb_serial_read_close(15)
