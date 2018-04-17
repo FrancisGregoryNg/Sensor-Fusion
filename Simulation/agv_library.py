ab775a3939959d3d0e6b933e545175d2d0055a64
Calvin <calvinforum@rocketmail.com>
2018-04-18 01:01:58 +0800

    Version 1.0

--- Simulation/agv_library.py
+++ Simulation/agv_library.py
@@ -1,3 +1,5 @@
+import random
+
 class Vehicle:
     def __init__(self, wheelRadius, mass, friction, dt, initialX = 0, initialY = 0, initialVx = 0, initialVy = 0, initialOmegaX = 0, initialOmegaY = 0):
         self.x = initialX
@@ -28,17 +30,23 @@ class Vehicle:
         dv = acceleration * self.dt
         
         if (abs(newVx - self.vx) > dv): #if the absolute difference between desired and actual velocity is greater than dv, increment velocity by dv
-            self.vx = self.vx + dv
+            self.vx = self.vx - dv
         else:                           #else (if the difference in desired vs actual is small) just set the velocity directly
             self.vx = newVx
             
         if (abs(newVx - self.vx) > dv):
-            self.vy = self.vy + dv
+            self.vy = self.vy - dv
         else:
             self.vy = newVy
-            
-        self.encoderX += self.omegaX * dt
-        self.encoderY += self.omegaY * dt
+        
+        self.x += self.vx * self.dt
+        self.y += self.vy * self.dt
+        
+        self.encoderX += self.omegaX * self.dt
+        self.encoderY += self.omegaY * self.dt
+        
+        self.ieee[0] = self.x + random.gauss(0, 1)
+        self.ieee[1] = self.y + random.gauss(0, 1)
             
     def readEncoder(self):              #encoder counts every 0.5 degrees, so convert from rad to deg, then multiply by 2
         countsX = 2 * self.encoderX * 180 / 3.14159265359
@@ -47,7 +55,21 @@ class Vehicle:
     def readIEEE(self):
         return self.ieee
         
-    def readActual(self);
+    def readActual(self):
         return (self.x, self.y)
-            
-roboman = Vehicle(0, 0, 0, 0, 0.05, 1, 0.5);
+
+#THE SEGMENT BELOW IS FOR TESTING PURPOSES ONLY==================================================
+roboman = Vehicle(0.05, 1, 0.5, 0.01, 0, 0, 1, 10, 0, 0);
+#0.05m wheel diameter
+#1kg mass
+#0.5 friction coefficient
+#0.01s timestep
+#1 m/s initial velocity x
+#10 m/s initial velocity y
+
+for x in range(0, 100):
+    print(x, end = " ")
+    print(*roboman.readActual())
+    #print(*roboman.readIEEE())
+    
+    roboman.updateModel();
